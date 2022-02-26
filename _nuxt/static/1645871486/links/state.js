371452531319733286ddb0856169ee5ff3412e63
ajcool2k <jens.auerswald@gmail.com>
2022-02-26 10:31:29 +0000

    Update from https://github.com/ajcool2k/blog/commit/b2fac6e27975f52e22de162a8b98f2360e360560

---
+++ _nuxt/static/1645871486/links/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645871486",layout:"default",error:a,serverRendered:b,routePath:"\u002Flinks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d462ac31"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
