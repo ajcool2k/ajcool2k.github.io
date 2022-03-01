94fe9ebbbabffeef44cc0a16b476fd04b0a461bf
ajcool2k <jens.auerswald@gmail.com>
2022-03-01 21:20:12 +0000

    Update from https://github.com/ajcool2k/blog/commit/3b9337816ed14d34415ea9f8143690d69346ba31

--- _nuxt/static/1646088571/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Flinks","\u002Ftags","\u002Fprojects","\u002Ftag\u002Fcrazy","\u002Fblog\u002Fmy-first-blog-post","\u002Ftag\u002Fhacking","\u002Fblog\u002Fblog-without-costs","\u002Fblog\u002Fauthor\u002FJens Auerswald"]})
--- _nuxt/static/1646088571/tag/hacking/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646088571",layout:"default",error:a,serverRendered:b,routePath:"\u002Ftag\u002Fhacking",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"443c7632"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
