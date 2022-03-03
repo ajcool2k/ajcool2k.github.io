08d3ce18c1dabc4e9b27c4407cfc418a93e05f11
ajcool2k <jens.auerswald@gmail.com>
2022-03-03 23:33:45 +0000

    Update from https://github.com/ajcool2k/blog/commit/91f736fd7b892dcdfaf5ab0236be6e3babec02e2

--- _nuxt/static/1646349223/blog/author/Jens Auerswald/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646349223",layout:"default",error:a,serverRendered:b,routePath:"\u002Fblog\u002Fauthor\u002FJens%20Auerswald",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ff4f8bcd"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
